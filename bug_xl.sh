9fa3697a394e3bfdda84c4bce3bdbf56dc1c0a66
xxkenxyz <84865104+xxkenxyz@users.noreply.github.com>
2022-10-07 14:12:18 +0700

    Create bug_xl.sh
    update kalau ada moddd cokk ini bug pointing yeee

---
+++ bug_xl.sh
@@ -0,0 +1,17 @@
+cdn.xxken.xyz
+xl1.xxken.xyz
+xl2.xxke.xyz
+xl sampek 20 isi sendri 
+
+api.xxken.xyz
+
+
+api xxken.xyz sampek 20 juga isi sendiri capek cokk
+
+lms.xxken.xyz
+
+test work 100% all metodd 
+
+via tjt tjg ssh vmess all konek kalau gak konek salah beartii proxy bisa make doman aja gak usah make ip 
+
+lebih di sarahkan make tjg dengan path stnadr bawaan akun kalian tnpaa wwss
